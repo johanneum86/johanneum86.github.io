0fd7d27db9974955f9babc648dfc9c403a00bb82
Katja Hoffmann <johanneum86@gmx.de>
2023-02-05 18:39:14 +0100

    1. Commit

---
+++ header.js
@@ -0,0 +1,11 @@
+function header(URL1,F1) 
+{ 
+  parent.frames[F1].location.href=URL1; 
+}
+
+function fenster()
+{
+
+ F1 = window.open("anfmulti.htm","Fenster1","width=310,height=400,scrollbars");
+}
+
